c31819a7a10bc7a47992147d4c9e8f702f4cad1b
Kevin Kelley <kkelley622@gmail.com>
2022-09-02 13:04:26 -0500

    Create state with list of songs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Navbar from "./components/navigation/Navbar";
 import Home from "./components/static/Home";
+import WantedSongs from "./components/songs/WantedSongs";
+import NewSong from "./components/songs/NewSong";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { baseUrl } from "./Global";
 
 const App = () => {
+  const [songs, setSongs] = useState([]);
+
+  useEffect(() => {
+    const fetchSongs = async () => {
+      const response = await fetch(baseUrl + "/songs");
+      const data = await response.json();
+      setSongs(data);
+    }
+    fetchSongs();
+  }, [])
+
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/songs" element={<WantedSongs songs= { songs }/>} />
+        <Route path="/songs/add" element={<NewSong />} />
+      </Routes>
+    </Router>
   );
 }
 
